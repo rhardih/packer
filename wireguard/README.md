588c4fb695fc2915bd08f198337c767717ca4525
René Hansen <renehh@gmail.com>
2021-04-10 23:20:52 +0200

    Updates WireGuard
    The run script now takes a -c parameter, which specifies the number of
    client configs to generate for the server. (Default 10)

--- wireguard/README.md
+++ wireguard/README.md
@@ -1,12 +1,16 @@
 # WireGuard
 
 This configuration sets up a [WireGuard](https://www.wireguard.com/) server on a
-[Digital Ocean](https://www.digitalocean.com/) droplet, in the *nyc1* zone.
+[Digital Ocean](https://www.digitalocean.com/) droplet image, in the *nyc1*
+zone.
 
-A script, [run.sh](./run.sh) is included, which will generate a public and
-private keys, as well as configurations for both server and client for those
-keys.
+A script, [run.sh](./run.sh) is included, which will generate a server
+configuration, with a number of client peers pre-specified, each of which will have a client configuration generate as well.
 
-The template requires and `API_TOKEN` environment variable to be set, which
-should be a [Digital Ocean API access
+The packer template requires an `API_TOKEN` environment variable to be set,
+which should be a [Digital Ocean API access
 token](https://cloud.digitalocean.com/account/api/tokens).
+
+After packer has run, create a new droplet off the newly created image and note
+the public ip address. Each client config will need to have `{SERVER-IP}`
+replaced with the droplets public ip.
--- wireguard/run.sh
+++ wireguard/run.sh
@@ -2,26 +2,96 @@
 
 set -euxo pipefail
 
-# Create client and server keys
-for dir in client server
-do
-  mkdir -p $dir
-  pushd $dir
+usage() {
+  echo "Usage: $0 [-c <number>]" 1>&2
+  echo
+  echo " -c Specifiy number of WireGuard clients to generate. Default 10"
+  exit 1
+}
 
-  wg genkey | tee privatekey | wg pubkey > publickey
+CLIENT_COUNT=10
+while getopts "hc:" opt; do
+  case $opt in
+    c ) CLIENT_COUNT=$OPTARG;;
+    h ) usage
+      exit 0;;
+    *) usage
+      exit 1;;
+  esac
+done
+
+echo "<$CLIENT_COUNT>"
+
+# Sanity check env
+
+for cmd in wg packer
+do
+  if ! command -v $cmd &> /dev/null
+  then
+    echo "$cmd could not be found"
+    exit
+  fi
+done
+
+# Required for Packer to connect to Digital Ocean
+echo $API_TOKEN > /dev/null
+
+# Create server keys
+mkdir -p server
+pushd server
+
+SERVER_PRIVATEKEY=$(wg genkey)
+SERVER_PUBLICKEY=$(wg pubkey <<< "$SERVER_PRIVATEKEY")
+
+# Create server config
+  cat > "wg0.conf" <<EOF
+[Interface]
+PrivateKey = $SERVER_PRIVATEKEY
+Address = 10.0.0.1/24
+PostUp = iptables -A FORWARD -i wg -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+ListenPort = 51820
+
+EOF
+
+popd
+
+# Create client keys
+mkdir -p client-configs
+pushd client-configs
+
+for n in $(seq 2 $(($CLIENT_COUNT + 1)))
+do
+  CLIENT_PRIVATEKEY=$(wg genkey)
+  CLIENT_PUBLICKEY=$(wg pubkey <<< "$CLIENT_PRIVATEKEY")
+
+  # Create client config
+  cat > "wg$n.conf" <<EOF
+[Interface]
+Address = 10.0.0.$n/32
+PrivateKey = $CLIENT_PRIVATEKEY
+DNS = 1.1.1.1
+
+[Peer]
+PublicKey = $SERVER_PUBLICKEY
+Endpoint = {SERVER-IP}:51820
+AllowedIPs = 0.0.0.0/0, ::/0
+EOF
+
+  # Add client as peer to server config
+  pushd ../server
+
+  cat >> "wg0.conf" <<EOF
+[Peer]
+PublicKey = $CLIENT_PUBLICKEY
+AllowedIPs = 10.0.0.$n/32
+
+EOF
 
   popd
 done
 
-# Create server config
-sed -e "s/{PRIVATEKEY}/$(sed 's/\//\\\//g' server/privatekey)/" \
-  -e "s/{PUBLICKEY}/$(sed 's/\//\\\//g' client/publickey)/" \
-  server/wg0.conf.tmpl > server/wg0.conf
-
-# Create client config
-sed -e "s/{PRIVATEKEY}/$(sed 's/\//\\\//g' client/privatekey)/" \
-  -e "s/{PUBLICKEY}/$(sed 's/\//\\\//g' server/publickey)/" \
-  client/wg0.conf.tmpl > client/wg0.conf
+popd
 
 # Run packer
 packer build wireguard.json
