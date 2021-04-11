f7098ad83fa1a53300d06a3a8795142fd0f0aa28
René Hansen <renehh@gmail.com>
2021-04-11 09:51:11 +0200

    Changes to proper indention in heredoc

--- wireguard/run.sh
+++ wireguard/run.sh
@@ -44,13 +44,13 @@ SERVER_PRIVATEKEY=$(wg genkey)
 SERVER_PUBLICKEY=$(wg pubkey <<< "$SERVER_PRIVATEKEY")
 
 # Create server config
-  cat > "wg0.conf" <<EOF
-[Interface]
-PrivateKey = $SERVER_PRIVATEKEY
-Address = 10.0.0.1/24
-PostUp = iptables -A FORWARD -i wg -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-PostDown = iptables -D FORWARD -i wg -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-ListenPort = 51820
+cat > "wg0.conf" <<- EOF
+	[Interface]
+	PrivateKey = $SERVER_PRIVATEKEY
+	Address = 10.0.0.1/24
+	PostUp = iptables -A FORWARD -i wg -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+	PostDown = iptables -D FORWARD -i wg -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+	ListenPort = 51820
 
 EOF
 
@@ -66,27 +66,27 @@ do
   CLIENT_PUBLICKEY=$(wg pubkey <<< "$CLIENT_PRIVATEKEY")
 
   # Create client config
-  cat > "wg$n.conf" <<EOF
-[Interface]
-Address = 10.0.0.$n/32
-PrivateKey = $CLIENT_PRIVATEKEY
-DNS = 1.1.1.1
+  cat > "wg$n.conf" <<- EOF
+		[Interface]
+		Address = 10.0.0.$n/32
+		PrivateKey = $CLIENT_PRIVATEKEY
+		DNS = 1.1.1.1
 
-[Peer]
-PublicKey = $SERVER_PUBLICKEY
-Endpoint = {SERVER-IP}:51820
-AllowedIPs = 0.0.0.0/0, ::/0
-EOF
+		[Peer]
+		PublicKey = $SERVER_PUBLICKEY
+		Endpoint = {SERVER-IP}:51820
+		AllowedIPs = 0.0.0.0/0, ::/0
+	EOF
 
   # Add client as peer to server config
   pushd ../server
 
-  cat >> "wg0.conf" <<EOF
-[Peer]
-PublicKey = $CLIENT_PUBLICKEY
-AllowedIPs = 10.0.0.$n/32
+  cat >> "wg0.conf" <<- EOF
+		[Peer]
+		PublicKey = $CLIENT_PUBLICKEY
+		AllowedIPs = 10.0.0.$n/32
 
-EOF
+	EOF
 
   popd
 done
