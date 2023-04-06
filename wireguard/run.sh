7f9b555106bf903fd9536375e20dd7de68bd2838
René Hansen <renehh@gmail.com>
2023-04-06 14:25:34 +0200

    Update run script
    To further automate the setup, this addition uses the Digital Ocean API
    to create the actual machine after the snapshot has been created, as
    well as update the client configs, with the correct server IP, after it
    has booted up.

--- wireguard/run.sh
+++ wireguard/run.sh
@@ -8,6 +8,26 @@ usage() {
   echo " -c Specifiy number of WireGuard clients to generate. Default 10"
 }
 
+##############################
+#  Sanity check environment  #
+##############################
+
+for cmd in wg packer jq
+do
+  if ! command -v $cmd &> /dev/null
+  then
+    echo "$cmd could not be found"
+    exit 1
+  fi
+done
+
+# Required for Packer to connect to Digital Ocean
+echo "$API_TOKEN" > /dev/null
+
+#########################################################
+#  Build a Ubuntu snapshot with WireGuard using Packer  #
+#########################################################
+
 CLIENT_COUNT=10
 while getopts "hc:" opt; do
   case $opt in
@@ -20,20 +40,6 @@ while getopts "hc:" opt; do
 done
 
 echo "<$CLIENT_COUNT>"
-
-# Sanity check env
-
-for cmd in wg packer
-do
-  if ! command -v $cmd &> /dev/null
-  then
-    echo "$cmd could not be found"
-    exit
-  fi
-done
-
-# Required for Packer to connect to Digital Ocean
-echo $API_TOKEN > /dev/null
 
 # Create server keys
 mkdir -p server
@@ -59,7 +65,8 @@ popd
 mkdir -p client-configs
 pushd client-configs
 
-for n in $(seq 2 $(($CLIENT_COUNT + 1)))
+# Start at 2 here, because 10.0.0.1 is the gateway, i.e. the server.
+for n in $(seq 2 "$CLIENT_COUNT")
 do
   CLIENT_PRIVATEKEY=$(wg genkey)
   CLIENT_PUBLICKEY=$(wg pubkey <<< "$CLIENT_PRIVATEKEY")
@@ -93,4 +100,60 @@ done
 popd
 
 # Run packer
-packer build wireguard.json
+packer build -machine-readable wireguard.json | tee build.output
+
+##################################################
+#  Create Droplet from snapshot built by packer  #
+##################################################
+
+# Get log line with the info we need
+log_line=$(grep 'digitalocean,artifact.*snapshot' build.output)
+log_line_value=$(echo "$log_line" | awk -F "," '{print $6}')
+
+# Fetch name, image id and region the snapshot was created with
+SNAPSHOT_NAME=$(echo "$log_line_value" | awk -F "'" '{print $2}')
+IMAGE_ID=$(echo "$log_line_value" | awk -F "[()]" '{print $2}' | awk '{ print $2}')
+REGION=$(echo "$log_line_value" | awk -F "'" '{print $4}')
+
+# Create a droplet from the newly created snapshot
+curl -X POST -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer '"$API_TOKEN"'' \
+  -d '{"name":"'"$SNAPSHOT_NAME"'-s-4vcpu-8gb-intel-nyc1-01",
+  "size":"s-4vcpu-8gb-intel",
+  "region":"'"$REGION"'",
+  "image":"'"$IMAGE_ID"'",
+  "vpc_uuid":"87a13848-0424-41da-9ba5-9acc55241ce1"}' \
+    "https://api.digitalocean.com/v2/droplets" | tee curl-0.output
+
+# Get the id of the newly created droplet
+DROPLET_ID=$(jq '.droplet.id' < curl-0.output)
+
+# Query the API to get the ip address of the newly created droplet, repeat if
+# the droplet has not yet booted and thus don't have an IP yet.
+RETRY_COUNT=0
+RETRY_LIMIT=10
+DROPLET_IP=""
+while [[ $RETRY_COUNT -lt $RETRY_LIMIT ]]; do
+  curl -X GET -H 'Content-Type: application/json' \
+    -H "Authorization: Bearer $API_TOKEN" \
+    "https://api.digitalocean.com/v2/droplets/$DROPLET_ID" | tee curl-1.output
+
+  DROPLET_IP=$(jq  '.droplet.networks.v4[0].ip_address' < curl-1.output | tr -d '"')
+
+  if [[ "$DROPLET_IP" == "null" ]]; then
+    echo "Droplet $DROPLET_ID has no IP yet. Sleeping 5s..."
+    sleep 10 # Delay to avoid spamming the server with requests
+
+    RETRY_COUNT=$((RETRY_COUNT+1))
+  else
+    break
+  fi
+done
+
+if [[ -z $DROPLET_IP ]]; then
+  echo "Failed to get IP of droplet $DROPLET_ID after $RETRY_LIMIT retries"
+else
+  # Replace the server IP placeholder in the client configs with the IP address of
+  # the newly created droplet
+  gsed -i "s/{SERVER-IP}/$DROPLET_IP/g" client-configs/*
+fi
