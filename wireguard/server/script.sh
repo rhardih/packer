1497acb5f838085b68ed59020fdf41a540f725de
René Hansen <renehh@gmail.com>
2024-04-13 13:55:52 +0200

    Avoid getting `apt upgrade` stuck

--- wireguard/server/script.sh
+++ wireguard/server/script.sh
@@ -2,9 +2,24 @@
 
 set -euxo pipefail
 
+# Make upgrades non interactive, to avoid prompts like this:
+#
+#  A new version (/tmp/tmp.7xIRMaLcC1) of configuration file /etc/ssh/sshd_config
+#  is available%!(PACKER_COMMA) but the version installed currently has been locally modified.
+#
+#    1. install the package maintainer's version
+#    2. keep the local version currently installed
+#    3. show the differences between the versions
+#    4. show a side-by-side difference between the versions
+#    5. show a 3-way difference between available versions
+#    6. do a 3-way merge between available versions
+#    7. start a new shell to examine the situation
+#
+export DEBIAN_FRONTEND=noninteractive
+
 # Install WireGuard
 apt update
-apt -y upgrade
+apt -yq upgrade
 apt -y install wireguard
 
 # Enable IP forwarding
