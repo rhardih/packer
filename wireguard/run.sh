5f0ee58265568642952abf5599bac276c59274e1
René Hansen <renehh@gmail.com>
2025-04-12 11:10:17 +0200

    Auto-format run.sh

--- wireguard/run.sh
+++ wireguard/run.sh
@@ -12,17 +12,15 @@ usage() {
 #  Sanity check environment  #
 ##############################
 
-for cmd in wg packer jq
-do
-  if ! command -v $cmd &> /dev/null
-  then
+for cmd in wg packer jq; do
+  if ! command -v $cmd &>/dev/null; then
     echo "$cmd could not be found"
     exit 1
   fi
 done
 
 # Required for Packer to connect to Digital Ocean
-echo "$API_TOKEN" > /dev/null
+echo "$API_TOKEN" >/dev/null
 
 #########################################################
 #  Build a Ubuntu snapshot with WireGuard using Packer  #
@@ -31,11 +29,15 @@ echo "$API_TOKEN" > /dev/null
 CLIENT_COUNT=10
 while getopts "hc:" opt; do
   case $opt in
-    c ) CLIENT_COUNT=$OPTARG;;
-    h ) usage
-      exit 0;;
-    *) usage
-      exit 1;;
+  c) CLIENT_COUNT=$OPTARG ;;
+  h)
+    usage
+    exit 0
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
   esac
 done
 
@@ -46,10 +48,10 @@ mkdir -p server
 pushd server
 
 SERVER_PRIVATEKEY=$(wg genkey)
-SERVER_PUBLICKEY=$(wg pubkey <<< "$SERVER_PRIVATEKEY")
+SERVER_PUBLICKEY=$(wg pubkey <<<"$SERVER_PRIVATEKEY")
 
 # Create server config
-cat > "wg0.conf" <<- EOF
+cat >"wg0.conf" <<-EOF
 	[Interface]
 	PrivateKey = $SERVER_PRIVATEKEY
 	Address = 10.0.0.1/24
@@ -66,13 +68,12 @@ mkdir -p client-configs
 pushd client-configs
 
 # Start at 2 here, because 10.0.0.1 is the gateway, i.e. the server.
-for n in $(seq 2 "$CLIENT_COUNT")
-do
+for n in $(seq 2 "$CLIENT_COUNT"); do
   CLIENT_PRIVATEKEY=$(wg genkey)
-  CLIENT_PUBLICKEY=$(wg pubkey <<< "$CLIENT_PRIVATEKEY")
+  CLIENT_PUBLICKEY=$(wg pubkey <<<"$CLIENT_PRIVATEKEY")
 
   # Create client config
-  cat > "wg$n.conf" <<- EOF
+  cat >"wg$n.conf" <<-EOF
 		[Interface]
 		Address = 10.0.0.$n/32
 		PrivateKey = $CLIENT_PRIVATEKEY
@@ -87,7 +88,7 @@ do
   # Add client as peer to server config
   pushd ../server
 
-  cat >> "wg0.conf" <<- EOF
+  cat >>"wg0.conf" <<-EOF
 		[Peer]
 		PublicKey = $CLIENT_PUBLICKEY
 		AllowedIPs = 10.0.0.$n/32
@@ -131,15 +132,15 @@ REGION=$(echo "$log_line_value" | awk -F "'" '{print $4}')
 # Create a droplet from the newly created snapshot
 curl -X POST -H 'Content-Type: application/json' \
   -H 'Authorization: Bearer '"$API_TOKEN"'' \
-  -d '{"name":"'"$SNAPSHOT_NAME"'-s-4vcpu-8gb-intel-nyc1-01",
-  "size":"s-4vcpu-8gb-intel",
+  -d '{"name":"'"$SNAPSHOT_NAME"'-s-1vcpu-512mb-10gb",
+  "size":"s-1vcpu-512mb-10gb",
   "region":"'"$REGION"'",
   "image":"'"$IMAGE_ID"'",
   "vpc_uuid":"87a13848-0424-41da-9ba5-9acc55241ce1"}' \
-    "https://api.digitalocean.com/v2/droplets" | tee curl-0.output
+  "https://api.digitalocean.com/v2/droplets" | tee curl-0.output
 
 # Get the id of the newly created droplet
-DROPLET_ID=$(jq '.droplet.id' < curl-0.output)
+DROPLET_ID=$(jq '.droplet.id' <curl-0.output)
 
 # Query the API to get the ip address of the newly created droplet, repeat if
 # the droplet has not yet booted and thus don't have an IP yet.
@@ -151,13 +152,13 @@ while [[ $RETRY_COUNT -lt $RETRY_LIMIT ]]; do
     -H "Authorization: Bearer $API_TOKEN" \
     "https://api.digitalocean.com/v2/droplets/$DROPLET_ID" | tee curl-1.output
 
-  DROPLET_IP=$(jq  '.droplet.networks.v4[0].ip_address' < curl-1.output | tr -d '"')
+  DROPLET_IP=$(jq '.droplet.networks.v4[0].ip_address' <curl-1.output | tr -d '"')
 
   if [[ "$DROPLET_IP" == "null" ]]; then
     echo "Droplet $DROPLET_ID has no IP yet. Sleeping 5s..."
     sleep 10 # Delay to avoid spamming the server with requests
 
-    RETRY_COUNT=$((RETRY_COUNT+1))
+    RETRY_COUNT=$((RETRY_COUNT + 1))
   else
     break
   fi
